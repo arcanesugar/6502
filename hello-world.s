ca1a74dababf4a5ffe9db9ba67aa919d72e4c712
j <thorntonjamesb@gmail.com>
2025-06-30 15:03:27 -0600

    Added vectors.s, removed lcd.s+hardware.s, hello-world.s now initialises lcd in 4 bit mode (requires slow clock)

--- hello-world.s
+++ hello-world.s
@@ -1,39 +1,69 @@
-  .org $8000 ; the following code begins at address $8000
-.include "hardware.s"
-.include "lcd.s"
-string:
-  .asciiz "  Hello World"
+.include "vectors.s"
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+
+LCD_E  = %10000000
+LCD_RW = %01000000
+LCD_RS = %00100000
+
+  .org $8000
 reset:
-  ldx #$ff
-  txs
-
-  lda #%11111111 ; Set all pins on port B to output
-  sta DDRB
-  lda #%11100000 ; Set top 3 pins on port A to output
+  lda #%11111111
   sta DDRA
-
-  lda #%00111000 ; 8-bit mode, 2-line display, 5x8 font
+  lda #%11111111
+  sta DDRB
+  
+  lda #%00101000 ; 4-bit; 2-lines; 5x8
   jsr lcd_instruction
-  lda #%00001110 ; Display on, cursor on, blink off
+  lda #%00001111 ; Display on; cursor on; blink on
   jsr lcd_instruction
-  lda #%00000110 ; Increment, don't shift display
+  lda #%00000110 ; Shift cursor
   jsr lcd_instruction
-  lda #%00000001 ; Return home
+  lda #$00000001 ; Clear display
   jsr lcd_instruction
-  lda #%00000001 ; Clear display
-  jsr lcd_instruction
-
-  ldx #0  
-print_loop:
-  lda string,x 
-  beq loop
-  jsr print_char
-  inx
-  jmp print_loop
 
 loop:
   jmp loop
-  .org $fffc ;the following code begins at address $fffc
-  .word reset
-  .word $0000
 
+lcd_instruction:
+  tax
+  lda #%11111111
+  sta DDRA
+  sta DDRB
+  txa
+  
+  ;store top of instruction in x
+  tay ;stash instruction in y
+  and #%11110000
+  tax
+  tya
+
+  ;store bottom of instruction in y
+  and #%00001111
+  asl
+  asl
+  asl
+  asl
+  tay
+  
+  txa
+  sta PORTB
+  lda #0
+  sta PORTA
+  lda #LCD_E
+  sta PORTA
+  lda #0
+  sta PORTA
+  
+  tya
+  sta PORTB
+  lda #0
+  sta PORTA
+  lda #LCD_E
+  sta PORTA
+  lda #0
+  sta PORTA
+
+  rts
---
+++ vectors.s
@@ -0,0 +1,4 @@
+  .org $fffa
+  .word $0000 ;NMIB
+  .word reset ;Reset
+  .word $0000 ;IRQB
